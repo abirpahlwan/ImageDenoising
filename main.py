90cde4fbadd33c7f52d484321b28b3e3274356de
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-11-04 00:55:37 +0600

    imported packages

---
+++ main.py
@@ -0,0 +1,15 @@
+﻿import matplotlib.pyplot as plt
+import numpy as np
+from scipy import ndimage
+from PIL import Image
+
+
+
+
+
+def main():
+    print("abc")
+
+
+if __name__ == '__main__':
+    main()
