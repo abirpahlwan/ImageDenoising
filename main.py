afc63242373cc88302b4dbc8bb82e3a47fd26b87
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-11-08 07:36:21 +0600

    improvement on hpf & lpf

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
 ﻿import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 from scipy import ndimage
-from PIL import Image
+
+import noise
+import kernel
 
 
 # File paths
-bird_image_path = r'data/bird.jpg'
-bird_with_noise_image_path = r'data/bird-with-noise.jpg'
-bird_with_noise_resized_image_path = r'data/bird-with-noise-resized.jpg'
+bird_image_path = r'data/birdo.jpg'
+# bird_with_noise_image_path = r'data/bird-with-noise.jpg'
+# bird_with_noise_resized_image_path = r'data/bird-with-noise-resized.jpg'
 
-# bird = np.array(Image.open(bird_image_path).resize(80, 36).convert('L'), dtype=np.uint8)
-# print(bird.shape)
-# plt.imshow(bird)
+bird_image = cv2.imread(bird_image_path, cv2.IMREAD_UNCHANGED)
+# cv2.imshow('bird_image', bird_image)
 
-bird_with_noise = np.array(Image.open(bird_with_noise_resized_image_path).convert('L'), dtype=np.uint8)
-# print(bird_with_noise.shape)
-# plt.imshow(bird_with_noise)
+bird_with_noise = noise.salt_pepper_v2(bird_image, 0.1)
+cv2.imshow('bird_with_noise', bird_with_noise)
 
-# HPF
-hpf_kernel_3x3 = np.array([[0,  1,  0],
-                           [1,  4,  1],
-                           [0,  1,  0]])
-
-hpf_kernel_5x5 = np.array([[-1, -1, -1, -1, -1],
-                           [-1, -1,  1, -1, -1],
-                           [-1,  1,  2,  1, -1],
-                           [-1, -1,  1, -1, -1],
-                           [-1, -1, -1, -1, -1]])
-
-# LPF
-lpf_kernel_3x3 = np.array([[-1, -1, -1],
-                           [-1,  8, -1],
-                           [-1, -1, -1]])
-
-
-# hpf_bird_with_noise = ndimage.convolve(bird_with_noise, hpf_kernel_3x3)
+hpf_bird_with_noise = ndimage.convolve(bird_with_noise, kernel.hpf_edge_kernel_3x3 / 9)
 # print(hpf_bird_with_noise.shape)
-# plt.imshow(hpf_bird_with_noise, cmap='gray')
-# Image.fromarray(hpf_bird_with_noise).save(r'data/hpf_bird_with_noise_4.jpg')
+cv2.imshow('hpf_bird_with_noise', hpf_bird_with_noise)
 
-lpf_bird_with_noise = ndimage.convolve(bird_with_noise, lpf_kernel_3x3)
-Image.fromarray(lpf_bird_with_noise).save(r'data/lpf_bird_with_noise.jpg')
+lpf_bird_with_noise = ndimage.convolve(bird_with_noise, kernel.lpf_average_kernel_5x5)
+# print(lpf_bird_with_noise.shape)
+cv2.imshow('lpf_bird_with_noise', lpf_bird_with_noise)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
