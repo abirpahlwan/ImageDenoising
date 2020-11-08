e088d062f4f00b092db9382018d0deb09d960f03
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-11-08 07:34:22 +0600

    external kernel module

---
+++ kernel.py
@@ -0,0 +1,26 @@
+﻿import numpy as np
+
+
+# HPF
+hpf_edge_kernel_3x3 = np.array([[-1, -1, -1],
+                                [-1,  8, -1],
+                                [-1, -1, -1]])
+
+hpf_basic_kernel_5x5 = np.array([[ 0, -1, -1, -1,  0],
+                                 [-1,  2, -4,  2, -1],
+                                 [-1, -4, 13, -4, -1],
+                                 [-1,  2, -4,  2, -1],
+                                 [ 0, -1, -1, -1,  0]])
+
+hpf_kernel_3x3 = np.array([[0,  1,  0],
+                           [1,  8,  1],
+                           [0,  1,  0]])
+
+hpf_kernel_5x5 = np.array([[-1, -1, -1, -1, -1],
+                           [-1, -1,  1, -1, -1],
+                           [-1,  1,  2,  1, -1],
+                           [-1, -1,  1, -1, -1],
+                           [-1, -1, -1, -1, -1]])
+
+# LPF
+lpf_average_kernel_5x5 = np.array(np.ones((5, 5), dtype=np.uint8))/25
